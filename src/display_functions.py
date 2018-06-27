55052f8772cb97fe938ccf273e0170c8e62c6d9c
Jeff Wilson <MythicalMeerkat@users.noreply.github.com>
2018-06-27 12:30:04 -0600

    Separated Main Script from Functions

--- src/display_functions.py
+++ src/display_functions.py
@@ -5,3 +5,13 @@ Start Date: 6/27/2018
 This python file hosts the display functions and UX improvements utilized by the main script.
 
 """
+
+
+def print_intro_instructions():
+    print("--- Water Marked Image Generator Script ---\n")
+    print("Author: Jeff Wilson\n")
+    print("This script can apply a user specified watermark template to another image and save it to a subfolder.\n")
+
+
+def display_time_elapsed(start, end):
+    print("\nTOTAL SCRIPT RUN TIME:", (end - start), "seconds")
